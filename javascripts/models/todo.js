fe57f4592582a44265e2443e26cbf2d2219b5725
Jay Shenk <jay.shenk@gmail.com>
2017-06-17 14:41:20 -0400

    Setup todo model

--- javascripts/models/todo.js
+++ javascripts/models/todo.js
@@ -1,3 +1,18 @@
 var Todo = Backbone.Model.extend({
-
+  defaults: {
+    complete: false
+  },
+  setDueDate: function() {
+    var newDueDate;
+    if (this.get("month") && this.get("year")) {
+      newDueDate = this.get("month") + "/" + this.get("year").slice(2, 4);
+    } else {
+      newDueDate = "No Due Date";
+    }
+    this.set("dueDate", newDueDate);
+  },
+  initialize: function() {
+    this.setDueDate();
+    this.on("change:month change:year", this.setDueDate);
+  }
 });
